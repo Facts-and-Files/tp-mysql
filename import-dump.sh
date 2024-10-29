25f341a3b8b7aeea1058cd3e7078e88e0b1f0b4b
trenc <github@tommyschmucker.de>
2024-10-29 09:14:02 +0100

    fix: remove version statement, latest import

--- import-dump.sh
+++ import-dump.sh
@@ -2,4 +2,4 @@
 
 export $(grep -v '^#' .env | xargs)
 
-sudo docker exec -i tp_mysql mysql -u"$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" <<< "source /root/live-transcribathon-2021-11-26.sql"
+sudo docker exec -i tp_mysql mysql -f -u"$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" <<< "source /root/live-transcribathon-2023-06-27.sql"
